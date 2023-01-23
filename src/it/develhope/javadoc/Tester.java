7b2b17b09811b3ba5bc4af7ddd81fec3d89cd46a
Alessio Limina <alessio.limina@gmail.com>
2023-01-23 12:39:44 +0100

    Exercise: javadoc-1

--- src/it/develhope/javadoc/Tester.java
+++ src/it/develhope/javadoc/Tester.java
@@ -1,2 +1,26 @@
-package it.develhope.javadoc;public class Tester {
+package it.develhope.javadoc;
+
+/**
+ * This class is a Tester class
+ * @author aless
+ */
+
+public class Tester {
+    public static void main(String[] args) {
+
+        System.out.println("-----------------Starting---------------------------");
+
+        /**
+         * We created a new WordGames object
+         */
+
+        WordGames wordgames = new WordGames();
+
+        System.out.println(wordgames.addHelloWorld("World!"));
+
+        System.out.println(wordgames.getFullName("Alessio", "Limina"));
+
+        System.out.println("------------------------------------------------------");
+
+    }
 }
--- src/it/develhope/javadoc/WordGames.java
+++ src/it/develhope/javadoc/WordGames.java
@@ -1,6 +1,32 @@
 package it.develhope.javadoc;
 
-public class Start {
+/**
+ * This class represents Word Games
+ * @author aless
+ */
+public class WordGames {
+
+    /**
+     * This method prints a word
+     * @param word is the word that we'll use
+     * @return is the return that we'll print
+     */
+
+    public String addHelloWorld(String word){
+        return "Hello " + word;
+    }
+
+    /**
+     * This method gets the full name
+     * @param name is the name the we'll use
+     * @param surname is the surname that we'll use
+     * @return is the return that we'll print
+     */
+
+    public String getFullName(String name, String surname) {
+        return name + " " + surname;
+
+
+    }
 }
 
-/*
